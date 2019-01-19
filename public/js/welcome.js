9b3168dcfa87db4b4814bbb577ee548e1af6f579
Vipin Yadav <imvpn22@gmail.com>
2019-01-19 19:06:29 +0530

    Added signup features

--- public/js/welcome.js
+++ public/js/welcome.js
@@ -3,29 +3,18 @@ const baseURL = `http://localhost:3000`;
 
 // Define a fetch function
 requestAPI = (url, data, callback) => {
-  console.log(data);
   fetch(url, {
     method: 'POST',
     headers: {
       "Content-type": "application/json"
     },
     body: JSON.stringify(data)
-  }).then(res => {
-    console.log(res);
-    return res.json()
-  }).then(res => {
-    console.log(res);
-    callback(res);
-  }).catch(err => {
-    console.log(err);
-
-    callback(err);
-  });
+  }).then(res => res.json())
+  .then(res => callback(null, res))
+  .catch(err => callback(err, null));
 }
 
 makeLogin = (e) => {
-  console.log('test');
-
   e.preventDefault();
   let data = {
     username: document.getElementById('li_uname').value,
@@ -35,14 +24,54 @@ makeLogin = (e) => {
   requestAPI(`${baseURL}/user/login`, data, (err, res) => {
     if (err) {
       console.error(err);
-
+      showError(err.SERVER_MESSAGE);
     } else {
       console.log(res);
+      showSuccess(res.SERVER_MESSAGE);
+
+      // TODO: Save USER and Redirect
 
     }
   });
 };
 
-document.getElementById('login_form').addEventListener('submit', e => makeLogin(e));
+makeSignup = e => {
+  e.preventDefault();
+  let data = {
+    name: document.getElementById('su_name').value,
+    email: document.getElementById('su_email').value,
+    username: document.getElementById('su_uname').value,
+    password: document.getElementById('su_pass').value,
+    isStudent: document.getElementById('su_user_type').value === 'admin' ? false : true,
+    isAdmin: document.getElementById('su_user_type').value === 'admin' ? true : false
+  };
 
+  console.log(data);
+
+  requestAPI(`${baseURL}/user/register`, data, (err, res) => {
+    if (err) {
+      console.error(err);
+      showError(err.SERVER_MESSAGE);
+    } else {
+      console.log(res);
+      showSuccess(res.SERVER_MESSAGE);
+    }
+  });
+
+
+};
+
+showError = (msg) => {
+  let resBox = document.getElementById('feedback_box');
+  resBox.innerHTML = msg;
+  resBox.classList.remove('hidden', 'success');
+  resBox.classList.add('error');
+}
+
+showSuccess = (msg) => {
+  let resBox = document.getElementById('feedback_box');
+  resBox.innerHTML = msg;
+  resBox.classList.remove('hidden', 'error');
+  resBox.classList.add('success');
+}
 
