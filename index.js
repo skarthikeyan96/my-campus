8022fe0680a081f766a947dc3c8c941a0c9bc7d1
karthikeyan <imkarthikeyans@gmail.com>
2019-01-20 00:22:31 +0530

    Added Feed model

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
-var path = require('path');
-var dotenv = require('dotenv');
-var createError = require('http-errors');
-var express = require('express');
-var cookieParser = require('cookie-parser');
+const path = require('path');
+const dotenv = require('dotenv');
+const createError = require('http-errors');
+const express = require('express');
+const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 // setting up the models
 let User = require('./models/user');
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-const taskRouter = require('./routes/task')
-var app = express();
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const taskRouter = require('./routes/task');
+const FeedRouter = require("./routes/feed");
+const app = express();
 // setting up the passport
 const session = require('express-session');
 const mongoose = require('mongoose');
@@ -55,6 +56,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/user', usersRouter);
 app.use('/task',taskRouter) // Routes to post the discusion
+app.use('/feed',FeedRouter)
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
