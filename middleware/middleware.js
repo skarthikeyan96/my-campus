069b53b98423f63116594db15ad7a9c16f95741d
Vipin Yadav <imvpn22@gmail.com>
2019-01-20 01:24:49 +0530

    Added fxn to CreateTask

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -36,7 +36,7 @@ module.exports = {
       return next()
     }
     //res.status(500).json({status:"error",message:"UnAuthorized Access"})
-    res.redirect('/login')
+    res.redirect('/user/login')
   },
   checkFeedOwner: (req, res, next) => {
     if (req.isAuthenticated()) {
